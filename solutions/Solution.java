eb1ac118695a21b36dce7f9e92be1898336db145
piedpipr <43669876+piedpipr@users.noreply.github.com>
2022-05-26 17:05:56 +0600

    Update

---
+++ solutions/Solution.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int x = scanner.nextInt();
+//        scanner.close();
+/////////////////////////////////////
+       //// Template For Problem Solving
+        //////////////////////
+
+
+
+        
+
+//////////////////////////////////////
+        //return status;
+    };
+
+}
+
+
+
+
